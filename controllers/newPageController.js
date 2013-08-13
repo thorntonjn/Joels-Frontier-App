9bd7519448880fef2f07d2c60a8445e7ad9e6a23
Joel Thornton <thorntonjn@familysearch.org>
2013-08-13 16:20:24 -0600

    Add toto list using angular.

---
+++ controllers/newPageController.js
@@ -0,0 +1,29 @@
+module.exports = function(app) {
+
+  app.get('/', function(req, res){
+    console.log("Joel's newPage");
+    res.render('index');
+  });
+
+  app.get('/newHeader', function (req, res) {
+    console.log("new header layout");
+    app.set("view options", {layout: "mylayout.ejs"});
+    res.render( 'newHeader' );
+
+  });
+
+  app.get('/angular', function (req, res) {
+    console.log("new header layout");
+    app.set("view options", {layout: "mylayout.ejs"});
+    res.render( 'angular' );
+
+  });
+
+  app.get('/:page', function(req, res){
+    console.log(":page-",req.params.page);
+    res.render( req.params.page );
+  });
+
+
+    
+};
--- controllers/routes.js
+++ controllers/routes.js
@@ -1,6 +1,7 @@
 module.exports = function(app) {
 
   app.get('/', function(req, res){
+    console.log("index");
     res.render('index');
   });
 
